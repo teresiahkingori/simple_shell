a88b1de29c4510ffcebaaadf0a16a7df1e5f5934
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-07-22 01:43:43 +0300

    Update shellheaderfile.h

--- shellheaderfile.h
+++ shellheaderfile.h
@@ -10,12 +10,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void print_environment(void)
-void exec_cmd(char **shll)
-void free_array(char **arr)
-int main(void)
-int str-int(char *str)
-void shell_loop(int is_interactive)
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-char *string_duplicate(char *str)
+void print_environment(void);
+void exec_cmd(char **shll);
+void free_array(char **arr);
+int main(void);
+int str-int(char *str);
+void shell_loop(int is_interactive);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *string_duplicate(char *str);
+void _puts(char *);
 
+#endif
