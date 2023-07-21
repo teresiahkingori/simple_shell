eb4e5d50f9dd3ccc3e8e18821bc2c6945fb8877a
safeya yasien <110609863+Safeya-Yasien@users.noreply.github.com>
2023-07-22 01:59:38 +0300

    Update shellheaderfile.h

--- shellheaderfile.h
+++ shellheaderfile.h
@@ -9,15 +9,23 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 
 void print_environment(void);
 void exec_cmd(char **shll);
 void free_array(char **arr);
 int main(void);
-int str-int(char *str);
+int str_to_int(char *str);
 void shell_loop(int is_interactive);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *string_duplicate(char *str);
+int _strcmp2(const char *s1, const char *s2);
+char **str_break(char *str, const char *delimiter);
+void execute_conditions(char **arguments, char *line);
+void free_arr(char **arr);
+void get_path(char **arguments);
+void execute_command(char **arguments);
+
 void _puts(char *);
 
 #endif
